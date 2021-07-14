37b32f6222c41204d063c622b8aabf85a86e3f8d
KateM3d <ekaterina8medvedeva@gmail.com>
2021-07-14 18:42:31 -0400

    better code no repeats

--- week13/index.js
+++ week13/index.js
@@ -1,4 +1,67 @@
+let num1;
+let num2;
+
+function enterNumber() {
+    num1 = Number(prompt('Enter First Number'));
+    num2 = Number(prompt('Enter Second Number'));
+}
+
 function calcPlus() {
+    enterNumber();
+    alert(num1 + num2);
+}
+
+function calcMinus() {
+    enterNumber();
+    alert(num1 - num2);
+}
+
+function calcDivide() {
+    enterNumber();
+    alert(num1 / num2);
+}
+
+function calcMultiply() {
+    enterNumber();
+    alert(num1 * num2);
+}
+
+
+
+/*function enterNumber() {
+    let num1 = prompt('Enter First Number');
+    let num2 = prompt('Enter Second Number');
+    plus = Number(num1) + Number(num2);
+    minus = Number(num1) - Number(num2);
+    divide = Number(num1) / Number(num2);
+    multiply = Number(num1) * Number(num2);
+    
+}
+
+function calcPlus() {
+    enterNumber();
+    alert(plus);
+}
+
+function calcMinus() {
+    enterNumber();
+    alert(minus);
+}
+
+function calcDivide() {
+    enterNumber();
+    alert(divide);
+}
+
+function calcMultiply() {
+    enterNumber();
+    alert(multiply);
+}
+
+
+*/
+
+/*function calcPlus() {
     let num1 = prompt('Enter First Number');
     let num2 = prompt('Enter Second Number');
     alert(Number(num1) + Number(num2));
@@ -20,4 +83,4 @@ function calcMultiply() {
     let num1 = prompt('Enter First Number');
     let num2 = prompt('Enter Second Number');
     alert(Number(num1) * Number(num2));
-}
+}*/
