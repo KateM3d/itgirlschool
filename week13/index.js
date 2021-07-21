40ce771f6c2044d496105a311ca3d0b2d54353d6
KateM3d <ekaterina8medvedeva@gmail.com>
2021-07-20 22:42:57 -0400

    added input fields

--- week13/index.js
+++ week13/index.js
@@ -1,40 +1,15 @@
-/*function calcPlus() {
-
-    let num1 = document.getElementById('numberOne').value;
-    if (num1.trim().length === 0 && Number.isNaN(Number(num1))) {
-
-        alert('Numbers only!');
-    }
-
-    let num2 = document.getElementById('numberTwo').value;
-    if (num2.trim().length === 0 && Number.isNaN(Number(num2))) {
-
-        alert('Numbers only!');
-    }
-
-
-    result = Number(num1) + Number(num2);
-    document.getElementById("result").value = result;
-
-}
-*/
-/*
-
 function enterNumber() {
 
     let num1 = document.getElementById('numberOne').value;
-
-    if (num1.trim().length === 0 && Number.isNaN(Number(num1))) {
-
-        alert('Numbers only!');
-    }
     let num2 = document.getElementById('numberTwo').value;
-    if (num2.trim().length === 0 && Number.isNaN(Number(num2))) {
-
+    if ((num1.trim().length === 0) || (num2.trim().length === 0) || Number.isNaN(Number(num1)) || Number.isNaN(Number(num2))) {
         alert('Numbers only!');
     }
-}
 
+
+    return [Number(num1), Number(num2)];
+
+}
 
 function calcPlus() {
 
@@ -42,30 +17,43 @@ function calcPlus() {
     let result = num1 + num2;
     document.getElementById("result").value = result;
 
+
 }
 
 function calcMinus() {
     let [num1, num2] = enterNumber();
     let result = num1 - num2;
     document.getElementById("result").value = result;
+
 }
 
 function calcDivide() {
     let [num1, num2] = enterNumber();
     let result = num1 / num2;
     document.getElementById("result").value = result;
+
 }
 
 function calcMultiply() {
     let [num1, num2] = enterNumber();
     let result = num1 * num2;
     document.getElementById("result").value = result;
+
+}
+
+function clearResult() {
+    let clearButton = document.getElementById("clear");
+    document.getElementById('numberOne').value = "";
+    document.getElementById('numberTwo').value = "";
+    document.getElementById("result").value = "";
 }
 
 
-enterNumber()
 
-
+/*function isValidNumber(value) {
+    return (value.trim().length !== 0) && (!Number.isNaN(Number(value)))
+}
+*/
 
 /*function enterNumber() {
     let num1 = prompt('Enter First Number');
