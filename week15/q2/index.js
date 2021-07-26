c8d2e8d8e07e3da8a643481adc931b9eee86f2d9
KateM3d <ekaterina8medvedeva@gmail.com>
2021-07-26 18:08:55 -0400

    added events

--- week15/q2/index.js
+++ week15/q2/index.js
@@ -1,17 +1,33 @@
-// const loginBtn = document.querySelector('.btn');
-
-// let firstName = document.getElementById('firstName');
-// let lastName = document.querySelector('#lastName');
-// let email = document.querySelector('#email');
-// let password = document.querySelector('#password');
-// let passwordConf = document.querySelector('#confPassword');
-
 function check() {
+
     let firstName = document.getElementById('firstName');
+    let lastName = document.querySelector('#lastName');
+    let email = document.querySelector('#email');
+    let password = document.querySelector('#password');
+    let passwordConf = document.querySelector('#confPassword');
+
     let message = document.querySelector('#errorMessage');
-    if (firstName.value === '') {
-        message.innerHTML += "your ${item} is missing<br>";
+    message.innerHTML = '';
+
+    if (!isValidEntry(firstName) || !isValidEntry(lastName) || !isValidEntry(email) || !isValidEntry(password) || !isValidEntry(passwordConf)) {
+
+        message.innerHTML += 'Oops! your are missing something'
     } else {
-        message.innerHTML += "Welcome!";
+        Swal.fire({
+            title: 'Awesome!',
+            text: 'Welcome!',
+            imageUrl: 'https://images.unsplash.com/photo-1600986600826-aa5b82e73a65?ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODh8fGhhcnJ5JTIwcG90dGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+            imageWidth: 400,
+            imageHeight: 300,
+            imageAlt: 'image',
+        });
     }
+}
+
+
+function isValidEntry(info) {
+    return info.value.trim() !== "";
+
+
+
 }
