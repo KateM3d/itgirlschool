0f20b4a93273c4191945ced7d6b7dd761a4e5416
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-07 09:30:15 -0400

    completed

--- week16/question1/script.js
+++ week16/question1/script.js
@@ -1,34 +1,15 @@
 function sumInput() {
-
+    let result = [];
     let sum = 0;
-    let value = [];
-    let i = 0;
-
     for (;;) {
-        i++;
-        //let result = value;
-        value = +prompt("Please enter number", '');
 
-        //if (!value) break;
-        if (i = 4) break;
-
+        let value = +prompt("Please enter number", '');
+        result.push(+value);
+        if (!value) break;
         sum += value;
     }
-    alert(value.sort());
-
+    result.pop();
+    alert(result.sort())
     alert('Total: ' + sum);
 }
-
 sumInput()
-
-
-
-// let result = [];
-// for (let i = 2; i <= 10; i++) {
-//     if (i % 2 === 0) {
-
-//         result.push(i);
-//     }
-
-// }
-// alert(result);
