e564cccdd645d39e008e408c8cca95ae768cc146
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-19 21:59:58 -0400

    removed extra

--- week17/question1/script.js
+++ week17/question1/script.js
@@ -1,35 +1,3 @@
 // Сделайте интерфейс преобразования ФИО на три поля (input). Учтите, пожалуйста, все нюансы - лишние пробелы, отсутствие больших букв в именах и пр. Результат должен получиться идеальным. Например, было введено пользователем "   Цветкова алиса АлександровНА  ", а стало три поля: 
 
 // Имя: * Алиса * , Фамилия: * Цветкова * , Отчество: * Александровна *
-
-
-// function caseCheck(userName) {
-//     if (!userName) return userName;
-
-//     return userName[0].toUpperCase() + userName.slice(1);
-// }
-
-// alert(caseCheck("kate"));
-
-// const randomNumber = Math.floor(Math.random() * 100) + 1;
-// console.log(randomNumber)
-
-// let date = new Date(2021, 11, 24, 22, 51);
-// console.log(date)
-
-
-// let now = new Date();
-
-// let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
-// let diff = tomorrow - now;
-// console.log(Math.round(diff / 1000));
-
-
-
-// let diff = Date.now() - date;
-// console.log(diff / 100 / 3600)
-
-// let amount = '$4534'
-// let onlyNumbers = parseInt(amount.match(/\d+/))
-
-// alert(onlyNumbers);
