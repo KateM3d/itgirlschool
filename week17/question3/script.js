4e558c3015a6de1541e52ba0a35c71114f7faee7
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-20 18:33:06 -0400

    added alert on every 6 secs

--- week17/question3/script.js
+++ week17/question3/script.js
@@ -1,18 +1,38 @@
-// Напишите функцию  formatDate(date), форматирующую  date  по следующему принципу:
-// - Если спустя `date` прошло менее 1 секунды, вывести `"прямо сейчас"`.
-// - В противном случае, если с `date` прошло меньше 1 минуты, вывести `"n сек. назад"`.
-// - В противном случае, если меньше часа, вывести `"m мин. назад"`.
-// - В противном случае, полная дата в формате `"DD.MM.YY HH:mm"`. А именно: `"день.месяц.год часы:минуты"`, всё в виде двух цифр, т.е. `31.12.16 10:00`.
+const date = new Date();
+alert(formatDate(date))
 
-//     Например:
+function showAlert() {
+    alert(formatDate(date))
+}
 
-//     ```jsx
-//     alert( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
+setInterval(showAlert, 6000);
 
-//     alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
 
-//     alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
+function formatDate(date) {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let diffMs = new Date() - date;
+    let diffSec = Math.round(diffMs / 1000);
+    let diffMin = Math.round(diffSec / 60);
+    let diffHour = Math.round(diffMin / 60);
 
-//     // вчерашняя дата вроде 31.12.2016, 20:00
-//     alert( formatDate(new Date(new Date - 86400 * 1000)) );
-//     ```
+
+    year = year.toString().slice(-2);
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+    hour = hour < 10 ? '0' + hour : hour;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    if (diffSec < 1) {
+        return 'right now'
+    } else if (diffMin < 1) {
+        return `${diffSec} seconds ago`
+    } else if (diffHour < 1) {
+        return `${diffMin} minutes ago`
+    } else {
+        return `${day}.${month}.${year} ${hour}:${minutes}`
+    }
+}
