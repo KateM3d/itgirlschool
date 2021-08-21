a2306eaa2e778957fab0d996c83605c78e655194
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-21 09:14:32 -0400

    added event on button, created new element li

--- week17/question2/script.js
+++ week17/question2/script.js
@@ -9,3 +9,15 @@
 // let comment3 = checkSpam("innocent rabbit"); //результат innocent rabbit
 
 // Подсказка: используйте пример с todo листом из групповой встречи.
+
+const inputField = document.querySelector('.inputMessage');
+const btn = document.querySelector('.btn');
+const newMessages = document.querySelector('.newMessages');
+
+btn.addEventListener('click', () => {
+    const message = document.createElement('li');
+    message.innerText = `New Message: ${inputField.value}`;
+    message.classList.add('toAddMessage');
+    newMessages.appendChild(message);
+    inputField.value = '';
+})
