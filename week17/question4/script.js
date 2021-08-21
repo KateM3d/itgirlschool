645e12df3942c1c932f49b20e6d8698f8f4fa066
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-20 21:15:47 -0400

    added function for finding min,max,avg,multi and sum of the array's elements

--- week17/question4/script.js
+++ week17/question4/script.js
@@ -1,15 +1,41 @@
-// Сделайте генератор 10 случайных чисел(по нажатию на кнопку) в диапазоне от - 10 до 10 и найдите среди них минимальное, максимальное и среднее арифметическое, а также сумму и произведение всех этих чисел.*В этой задаче вам понадобятся массивы и циклы.*
+const par = document.querySelector('#par');
+const btn = document.querySelector('#btn');
 
-// Пример.
+btn.addEventListener('click', generateRandomNums);
 
-// *Сгенерировали: -9 1 8 -2 3 2 0 1 -5 7*
 
-// *Минимальное: -9*
+function generateRandomNums() {
+    let res = document.querySelector('.results');
+    let nums = [];
 
-// *Максимальное: 8*
+    for (let i = 0; i < 10; i++) {
+        let randomNum = Math.floor(Math.random() * 21) - 10;
+        nums.push(randomNum);
+    }
 
-// *Среднее арифметическое: 0.6*
+    res.style.display = "block";
+    par.innerHTML = nums;
+    arrayResults(nums);
+}
 
-// *Сумма чисел: 6*
+function arrayResults(nums) {
+    let min = document.querySelector('#min');
+    let max = document.querySelector('#max');
+    let avg = document.querySelector('#avg');
+    let sum = document.querySelector('#sum');
+    let multi = document.querySelector('#multi');
+    let sumTotal = 0;
 
-// *Произведение чисел: 0*
+    for (let i = 0; i < nums.length; i++) {
+        sumTotal += nums[i];
+    }
+
+    let avgTotal = sumTotal / nums.length;
+    let multiTotal = nums.reduce((a, b) => a * b)
+
+    min.innerHTML = `<span>min:</span> ${Math.min.apply(null, nums)}`;
+    max.innerHTML = `<span>max:</span> ${Math.max.apply(null, nums)}`;
+    sum.innerHTML = `<span>sum:</span> ${sumTotal}`;
+    avg.innerHTML = `<span>avg:</span> ${avgTotal}`;
+    multi.innerHTML = `<span>multi:</span> ${multiTotal}`;
+}
