ba56df4e872b45417ac7debc47eeea2892a3f167
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-22 13:31:54 -0400

    works checks for 1st and 2nd word but not together

--- week17/question2/script.js
+++ week17/question2/script.js
@@ -1,66 +1,49 @@
-// Сделайте интерфейс для анонимных комментариев со спамфильтром.
-// Необходимо реализовать отображение и добавление сообщений, а также функцию  `checkSpam(str)`, заменяющую  `'viagra'` 
-// или  `'XXX'`
-// на `***`
-
-// Функция должна быть нечувствительна к регистру:
-// let comment1 = checkSpam('buy ViAgRA now'); //результат buy *** now
-// let comment2 = checkSpam('free xxx'); //результат free ***
-// let comment3 = checkSpam("innocent rabbit"); //результат innocent rabbit
-
-// Подсказка: используйте пример с todo листом из групповой встречи.
-
 const inputField = document.querySelector('.inputMessage');
 const btn = document.querySelector('.btn');
 const newMessages = document.querySelector('.newMessages');
 
-btn.addEventListener('click', entryCheck);
+btn.addEventListener('click', postMessage);
 
 
-function entryCheck() {
-    const message = document.createElement('li');
-
+function postMessage() {
 
     if (inputField.value.trim().length === 0) {
         inputField.value = '';
         alert('Please enter your message');
     } else {
-        checkSpam(message);
-        // message.innerText = `New Message: ${inputField.value}`;
-        // message.classList.add('toAddMessage');
-
-        // newMessages.appendChild(message);
-        // inputField.value = '';
+        const message = document.createElement('li');
+        let messageContent = inputField.value.toLowerCase();
+        checkSpam(message, messageContent);
     }
 }
 
 
-function checkSpam(message) {
+function checkSpam(message, messageContent) {
 
-    let messageContent = inputField.value;
-
-    // let messageContent = message.innerText;
-    let newInputField = messageContent.replace('viagra', '***');
     if (messageContent.includes('viagra')) {
-        // alert('yes');
+        let newInputField = messageContent.replace('viagra', '***');
         message.innerText = `New Message: ${newInputField}`;
-        message.classList.add('toAddMessage');
-
-        newMessages.appendChild(message);
-        inputField.value = '';
+        postCheckedMessage(message, newMessages, inputField);
         alert(newInputField)
-
+    } else if (messageContent.includes('xxx')) {
+        let newInputField = messageContent.replace('xxx', '***');
+        message.innerText = `New Message: ${newInputField}`;
+        postCheckedMessage(message, newMessages, inputField);
+        alert(newInputField)
     } else {
-        alert('no');
         message.innerText = `New Message: ${inputField.value}`;
-        message.classList.add('toAddMessage');
-
-        newMessages.appendChild(message);
-        inputField.value = '';
+        postCheckedMessage(message, newMessages, inputField);
+        alert('no');
     }
-
 }
 
+
+
+function postCheckedMessage(message, newMessages, inputField) {
+    message.classList.add('toAddMessage');
+    newMessages.appendChild(message);
+    inputField.value = '';
+}
 
 //     messageContent.replace(/viagra/ig, "***") : (message.innerText = `New Message: ${inputField.value}`);
 // message.replace(/xxx/ig, "***");
