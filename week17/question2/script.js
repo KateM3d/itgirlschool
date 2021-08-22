464dc21b3e94d7244fa721c28af5a549902ec187
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-21 21:34:36 -0400

    trying to build checkSpam function

--- week17/question2/script.js
+++ week17/question2/script.js
@@ -4,7 +4,7 @@
 // на `***`
 
 // Функция должна быть нечувствительна к регистру:
-//     let comment1 = checkSpam('buy ViAgRA now'); //результат buy *** now
+// let comment1 = checkSpam('buy ViAgRA now'); //результат buy *** now
 // let comment2 = checkSpam('free xxx'); //результат free ***
 // let comment3 = checkSpam("innocent rabbit"); //результат innocent rabbit
 
@@ -14,10 +14,46 @@ const inputField = document.querySelector('.inputMessage');
 const btn = document.querySelector('.btn');
 const newMessages = document.querySelector('.newMessages');
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', entryCheck);
+
+
+
+function entryCheck() {
     const message = document.createElement('li');
-    message.innerText = `New Message: ${inputField.value}`;
-    message.classList.add('toAddMessage');
-    newMessages.appendChild(message);
-    inputField.value = '';
-})
+
+
+    if (inputField.value === '') {
+        alert('Please enter your message');
+    } else {
+
+        message.innerText = `New Message: ${inputField.value}`;
+        message.classList.add('toAddMessage');
+        checkSpam(message);
+        newMessages.appendChild(message);
+        inputField.value = '';
+    }
+
+
+
+}
+
+
+function checkSpam(message) {
+    let messageContent = message.innerText;
+    let newInputField = messageContent.replace('viagra', '***');
+    if (messageContent.includes('viagra')) {
+        // alert('yes');
+        messageContent = `New Message: ${newInputField}`;
+        alert(newInputField)
+
+    } else {
+        alert('no');
+    }
+
+    //     messageContent.replace(/viagra/ig, "***") : (message.innerText = `New Message: ${inputField.value}`);
+    // message.replace(/xxx/ig, "***");
+
+}
+
+
+// || messageContent.includes(/xxx/i)
