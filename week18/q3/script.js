4b455484ab36bbcaaf0b7e9f53de1ea052b42503
KateM3d <ekaterina8medvedeva@gmail.com>
2021-09-02 18:04:24 -0400

    added spamcheck

--- week18/q3/script.js
+++ week18/q3/script.js
@@ -1,1 +1,44 @@
 // Доработать сохранение комментариев из прошлого домашнего задания через веб-хранилище, дать возможность пользователю сохранять свое имя и аватарку при вводе и запоминать их.
+
+const inputField = document.querySelector('.inputMessage');
+const btn = document.querySelector('.btn');
+const newMessages = document.querySelector('.newMessages');
+let newInputField;
+const badWordsCollection = ['viagra', 'xxx'];
+btn.addEventListener('click', postMessage);
+
+function postMessage() {
+
+    if (inputField.value.trim().length === 0) {
+        inputField.value = '';
+        alert('Please enter your message');
+    } else {
+        let str = inputField.value;
+        let trimmedStr = str.trim();
+        let inputWords = trimmedStr.trim().replace(/\s+/g, " ").split(' ');
+        console.log(inputWords)
+        const message = document.createElement('li');
+
+        for (let i = 0; i < badWordsCollection.length; i++) {
+            replaceBadWord(inputWords, badWordsCollection[i]);
+        }
+
+        newInputField = inputWords.join(' ');
+        message.innerText = `New Message: ${newInputField}`;
+        postCheckedMessage(message, newMessages);
+        inputField.value = '';
+    }
+}
+
+function replaceBadWord(inputWords, badWord) {
+    for (let i = 0; i < inputWords.length; i++) {
+        if (inputWords[i].toLowerCase() === badWord.toLowerCase()) {
+            inputWords[i] = "***";
+        }
+    }
+}
+
+function postCheckedMessage(message, newMessages) {
+    message.classList.add('toAddMessage');
+    newMessages.appendChild(message);
+}
