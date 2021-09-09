3e8506807283fc62512fd34dc32db9f556837d1b
KateM3d <ekaterina8medvedeva@gmail.com>
2021-09-09 16:12:38 -0400

    static class works

--- week19/question3/script.js
+++ week19/question3/script.js
@@ -1,1 +1,23 @@
 // Создайте класс Калькулятор, который будет содержать 4 статических метода - Сложение, Умножение, Вычитание и Деление. Доработайте свою страничку с калькулятором, чтобы в ней использовались методы этого класса.
+
+class CalculatorSatic {
+    static Sum(a, b) {
+        return a + b;
+    }
+    static Minus(a, b) {
+        return a - b;
+    }
+    static Mult(a, b) {
+        return a * b
+    }
+
+    static Div(a, b) {
+        return a / b
+    }
+}
+
+
+console.log(CalculatorSatic.Sum(6, 3))
+console.log(CalculatorSatic.Minus(6, 3))
+console.log(CalculatorSatic.Mult(6, 3))
+console.log(CalculatorSatic.Div(6, 3))
