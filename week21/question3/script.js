f42978c36b9c939ebc512f40b4587c9a2df61d3d
KateM3d <ekaterina8medvedeva@gmail.com>
2021-09-24 16:30:13 -0400

    added method Post

---
+++ week21/question3/script.js
@@ -0,0 +1,34 @@
+const myBtn = document.querySelector('#btn');
+
+
+myBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+
+    let user = {
+        petName: document.querySelector('#petsName').value,
+        ownerName: document.querySelector('#ownersName').value,
+        ownersPhone: phone,
+        breed: document.querySelectorAll('.breed').selected,
+        food: document.querySelectorAll('.food').checked,
+        gender: document.querySelectorAll('.gender').checked,
+        comment: document.querySelector('#comment').value,
+        photo: document.querySelector('.photo')
+
+    }
+    console.log(user)
+    fetch("https://httpbin.org/post", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+    })
+
+    .then(response => response.json())
+        .then(user => {
+            console.log(user);
+        })
+        .catch(error => console.log(error));
+
+})
