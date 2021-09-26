015968a6a22cd88364983281a492b20779fef9e4
KateM3d <ekaterina8medvedeva@gmail.com>
2021-09-26 09:53:27 -0400

    added FormData

--- week21/question3/script.js
+++ week21/question3/script.js
@@ -8,7 +8,7 @@ myBtn.addEventListener('click', (e) => {
     let user = {
         petName: document.querySelector('#petsName').value,
         ownerName: document.querySelector('#ownersName').value,
-        ownersPhone: phone,
+        ownersPhone: document.querySelector('#ownersPhone').value,
         breed: document.querySelectorAll('.breed').selected,
         food: document.querySelectorAll('.food').checked,
         gender: document.querySelectorAll('.gender').checked,
@@ -19,10 +19,11 @@ myBtn.addEventListener('click', (e) => {
     console.log(user)
     fetch("https://httpbin.org/post", {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(user)
+        body: new FormData(document.querySelector('#form')),
+        // headers: {
+        //     'Content-Type': 'application/json;charset=utf-8'
+        // },
+        // body: JSON.stringify(user)
     })
 
     .then(response => response.json())
